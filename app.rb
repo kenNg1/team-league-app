0c7aab734b2934c2829d13ca981336596f1b04e5
kenNg1 <kng1230@hotmail.com>
2017-04-11 17:17:48 +0800

    implemented javascript to team page

--- app.rb
+++ app.rb
@@ -39,20 +39,36 @@ end
 get("/teams/:id") do
   @team = Team.find(params.fetch('id').to_i)
   @teams= Team.all()
+  @games = @team.game_as_team1 + @team.game_as_team2
   erb(:team)
 end
+
+# a href (/teams/17/home)
+# params.fetch(home_or_away) --> home
+# a href (/teams/17/away)
+# params.fetch(home_or_away) --> away
+# http query parameters
+#
+# http://localhost:4567/teams/17
+# a href (/teams/17/?add_home=true)
+# params.fetch(add_home, false)
+# a href (/teams/17/?add_away=true)
+# params.fetch(add_away, false)
+
   get("/teams/:id/add_home") do
     @team = Team.find(params.fetch('id').to_i)
     @teams= Team.all()
+    @games = @team.game_as_team1 + @team.game_as_team2
     @add_home = true
     erb(:team)
-    end
+  end
   get("/teams/:id/add_away") do
     @team = Team.find(params.fetch('id').to_i)
     @teams= Team.all()
+    @games = @team.game_as_team1 + @team.game_as_team2
     @add_home = false
     erb(:team)
-    end
+  end
   post("/games") do
     team1_id = params.fetch('team1_id')
     team2_id = params.fetch('team2_id')
@@ -60,9 +76,17 @@ end
     self_id = params.fetch('self_id')
     redirect ("/teams/#{self_id}")
     end
-  # post("/games/add_away") do
-  #   team1_id = params.fetch('team1_id')
-  #   team2_id = params.fetch('team2_id')
-  #   game = Game.create({:team1_id => team1_id, :team2_id => team2_id })
-  #   redirect ("/teams/#{team1_id}")
-  #   end
+  patch("/games") do
+    game = Game.find(params.fetch('game_id').to_i())
+    team1_score = params.fetch('team1_score')
+    team2_score = params.fetch('team2_score')
+    game.update({:team1_score => team1_score, :team2_score => team2_score })
+    redirect ("/")
+  end
+  get("/teams/:id/add_result") do
+    @team = Team.find(params.fetch('id').to_i)
+    @teams= Team.all()
+    @games = @team.game_as_team1 + @team.game_as_team2
+    @add_result = true
+    erb(:team)
+  end
